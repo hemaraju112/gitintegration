2d668d6209bc47e188b4c2cce488ca59322729a0
hemaraju112 <84305723+hemaraju112@users.noreply.github.com>
2022-01-04 14:29:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
-print("welcome to LG!!!----fh")
-print("hi:")
-print("done-kdklstgffg")
-print("rashmip")
+string1 = "Linux"
+string2 = "Hint"
+joined_string = string1 + string2
+print(joined_string))
